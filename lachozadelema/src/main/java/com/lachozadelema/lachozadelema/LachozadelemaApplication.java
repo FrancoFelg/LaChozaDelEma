faa40d16a11d885868f2c8f4786e95154ff4bd7e
Franco Lamberti <franco_lamberti_02@yahoo.com.ar>
2021-12-21 15:16:39 -0300

    primer commit

---
+++ lachozadelema/src/main/java/com/lachozadelema/lachozadelema/LachozadelemaApplication.java
@@ -0,0 +1,13 @@
+package com.lachozadelema.lachozadelema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LachozadelemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LachozadelemaApplication.class, args);
+	}
+
+}
---
+++ lachozadelema/src/test/java/com/lachozadelema/lachozadelema/LachozadelemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lachozadelema.lachozadelema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LachozadelemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
